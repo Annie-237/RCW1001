58258db49b9a4dc994605f2c946db523cd73db67
Annie-237 <anniedjoukouo59@gmail.com>
2025-06-03 08:15:58 -0400

    rcw3
    rcw

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import requests 
-url = 'http"//localhost:8000'
+#url = 'http://localhost:8000'
+url = 'https://appi-ghfwdsbkd8a2d7cj.canadaeast-01.azurewebsites.net'
 
 response = requests.get(url)
 response = response.json()
